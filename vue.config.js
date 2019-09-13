4fbbef26f02bf71b541631c83e95aea72887dd31
Daniel Gonzalez <daniel.gonzalez9115@gmail.com>
2019-09-12 21:15:39 -0700

    added fix for cannot read property name of undefined from webpack sizelimitsplugin module

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 module.exports = {
+  // fix for issue related to cannot read property of name of undefined from webpack sizelimitsplugin - https://github.com/vuejs/vue-cli/issues/4572
+  css: {
+    sourceMap: true
+  },
+  productionSourceMap: false,
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "scss",
